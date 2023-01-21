b918659fc91b6eb4fc02a4060fe2445c46a69251
Vulcan <vulcan.lee@gmail.com>
2023-01-22 00:20:43 +0800

    加入 INavigationAware 介面

--- src/PrismApp1/PrismApp1/ViewModels/MainPageViewModel.cs
+++ src/PrismApp1/PrismApp1/ViewModels/MainPageViewModel.cs
@@ -3,7 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace PrismApp1.ViewModels;
 
-public partial class MainPageViewModel : ObservableObject
+public partial class MainPageViewModel : ObservableObject, INavigatedAware
 {
     private int _count;
 
@@ -27,5 +27,13 @@ public partial class MainPageViewModel : ObservableObject
             Text = "Clicked 1 time";
         else if (_count > 1)
             Text = $"Clicked {_count} times";
+    }
+
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+    }
+
+    public void OnNavigatedTo(INavigationParameters parameters)
+    {
     }
 }
